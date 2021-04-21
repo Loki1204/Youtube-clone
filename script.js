805d09d77442fca7aa9c8fbc29c9a6957c922f9a
Loki1204 <78527617+Loki1204@users.noreply.github.com>
2021-04-21 11:43:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
-
-const CLIENT_ID = '384795757441-1r3ui08t7vib6nrn369qn8d0efm71c31.apps.googleusercontent.com'
-const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'];
-const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/youtube.upload';
-
-const authorizeButton = document.getElementById('authorize-button');
-const signoutButton = document.getElementById('signout-button');
-const content = document.getElementById('content');
-const channelForm = document.getElementById('channel-form');
-const channelInput = document.getElementById('channel-input');
-const videoContainer = document.getElementById('video-container');
-
-const defaultChannel = 'ted';
+const loginBtn = document.getElementById('login-btn');
+const logoutBtn = document.getElementById('logout-btn');
 
 
-function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
-}
-
-
-function initClient(){
-  gapi.client.init({
-    discoveryDocs: DISCOVERY_DOCS,
-    clientId: CLIENT_ID,
-    scope: SCOPES
-  }).then(() => {
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus); 
-    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    authorizeButton.onclick = handleAuthClick;
-    signoutButton.onclick = handleSignoutClick;
-  })
-}
-
-function updateSigninStatus(isSignedIn) {
-  if (isSignedIn) {
-    authorizeButton.style.display = 'none';
-    signoutButton.style.display = 'block';
-    content.style.display = 'block';
-    videoContainer.style.display = 'block';
-    getChannel(defaultChannel);
-  } else {
-    authorizeButton.style.display = 'block';
-    signoutButton.style.display = 'none';
-    content.style.display = 'none';
-    videoContainer.style.display = 'none';
+var GoogleAuth;
+var SCOPE = 'https://www.googleapis.com/auth/youtube.force-ssl';
+  
+  function handleClientLoad() {
+    
+    gapi.load('client:auth2', initClient);
   }
-}
 
-function handleAuthClick() {
-  gapi.auth2.getAuthInstance().signIn();
-}
+  function initClient() {
+    var discoveryUrl = 'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest';
+    gapi.client.init({
+        'apiKey': 'AIzaSyCMgbcwlUdRdlohbLQKReG0MtDtNu9f7pE',
+        'clientId': '384795757441-1r3ui08t7vib6nrn369qn8d0efm71c31.apps.googleusercontent.com',
+        'discoveryDocs': [discoveryUrl],
+        'scope': SCOPE
+    }).then(function () {
+      GoogleAuth = gapi.auth2.getAuthInstance();
 
-function handleSignoutClick() {
-  gapi.auth2.getAuthInstance().signOut();
-}
+      GoogleAuth.isSignedIn.listen(updateSigninStatus);
+      var user = GoogleAuth.currentUser.get();
+      setSigninStatus();
 
-function getChannel(channel){
-  console.log(channel)
-}
+      loginBtn.onclick = handleAuthClick;
+      logoutBtn.onclick = handleAuthClick;
+    });
+      
+    
+      function handleAuthClick() {
+        if (GoogleAuth.isSignedIn.get()) {
+          GoogleAuth.signOut();
+        } else {
+          GoogleAuth.signIn();
+        }
+      }
+
+      function revokeAccess() {
+        GoogleAuth.disconnect();
+      }
+
+      function setSigninStatus() {
+        var user = GoogleAuth.currentUser.get();
+        var isAuthorized = user.hasGrantedScopes(SCOPE);
+        if (isAuthorized) {
+            loginBtn.style.display = 'none'
+            logoutBtn.style.display = 'block'
+        } else {
+          loginBtn.style.display = 'blcok'
+          logoutBtn.style.display = 'none'
+        }
+      }
+
+      function updateSigninStatus() {
+        setSigninStatus();
+      }
+    }
