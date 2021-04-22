15cf497ff9de53deb13a6ee1653d4a2ab3ed5167
Loki1204 <78527617+Loki1204@users.noreply.github.com>
2021-04-22 17:51:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -143,8 +143,9 @@ var SCOPE = 'https://www.googleapis.com/auth/youtube https://www.googleapis.com/
       if(playListItems){
         
         
-        let output  = `<br><h4 style="text-align: center;">Latest Vidoes</h4> <iframe width="560" height="315" src="https://www.youtube.com/embed/${playListItems[0].snippet.resourceId.videoId}" title="YouTube video player" frameborder="0" 
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen id="video-player"></iframe>`;
+        let output  = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${playListItems[0].snippet.resourceId.videoId}" title="YouTube video player" frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen id="video-player"></iframe>
+                          <br><h4 style="text-align: center;">Latest Vidoes</h4> `;
 
         playListItems.forEach(item =>{
           const videoId = item.snippet.resourceId.videoId;
